a297f99ac2e935328876d52bcc285e3b90aeda2a
dominiqq <dominik@gmail.com>
2016-12-10 20:53:59 +0100

    adasd

--- app/src/main/java/com/github/panda3/panda3player/NavDrawerActivity.java
+++ app/src/main/java/com/github/panda3/panda3player/NavDrawerActivity.java
@@ -1,8 +1,12 @@
 package com.github.panda3.panda3player;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,6 +28,25 @@ public class NavDrawerActivity extends AppCompatActivity
     private String currentlyPlayedVideoUri;
     private int currentlyPlayedVideoPosition = 0;
 
+    Context context;
+    Intent intentBindService;
+    private BoundService myServiceBinder;
+    boolean mBound = false;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+
+            Log.d("ServiceConnection","connected");
+            BoundService.MyBinder binder = (BoundService.MyBinder) service;
+            myServiceBinder = binder.getService();
+            mBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+
+            mBound = false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +55,9 @@ public class NavDrawerActivity extends AppCompatActivity
         //toolbar.setTitle("PanDa3 Player");
         setSupportActionBar(toolbar);
 
+        context = getApplicationContext();
+        intentBindService = new Intent(context,BoundService.class);
+        bindService(intentBindService, mConnection, Context.BIND_AUTO_CREATE);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
